ef8178a38c44fccfbf9457a0348a0662ecc716bb
Daniel Souza <danielrsouza10@gmail.com>
2023-09-18 10:12:42 -0300

    dynamic input the content of the big card done

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-big-card',
   templateUrl: './big-card.component.html',
-  styleUrls: ['./big-card.component.css']
+  styleUrls: ['./big-card.component.css'],
 })
 export class BigCardComponent {
-
+  @Input()
+  imgSrc: string = '/assets/images/bt-1.jpg';
+  @Input()
+  gameType: string = 'Full Game';
+  @Input()
+  gameConsole: string = 'PS4';
+  @Input()
+  gamePrice: string = '$59.99';
 }
