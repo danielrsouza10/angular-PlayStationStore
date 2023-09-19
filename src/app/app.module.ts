a2d390f69f3774a5759be1b60b3f64ca4293987c
Daniel Souza <danielrsouza10@gmail.com>
2023-09-18 22:48:56 -0300

    project finished

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,27 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
-import { MainNewsComponent } from './components/main-news/main-news.component';
+
 import { CardComponent } from './components/card/card.component';
-import { BigCardComponent } from './components/big-card/big-card.component';
-import { CardLabelComponent } from './components/card-label/card-label.component';
-import { CardPriceComponent } from './components/card-price/card-price.component';
+
+import { CardLabelComponent } from './components/card/card-label/card-label.component';
+import { CardPriceComponent } from './components/card/card-price/card-price.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuBarComponent,
-    MainNewsComponent,
     CardComponent,
-    BigCardComponent,
+
     CardLabelComponent,
-    CardPriceComponent
+    CardPriceComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.css']
+  styleUrls: ['./card.component.css'],
 })
 export class CardComponent {
-
+  @Input()
+  cardImage: string = '';
+  @Input()
+  cardLabel: string = '';
+  @Input()
+  cardTitle: string = '';
+  @Input()
+  cardConsole: string = '';
+  @Input()
+  cardPrice: string = '';
 }
